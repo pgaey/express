ab93f88a868eb13d6e9a413c664036192f53e0d8
evan <evan@nextpay.co.kr>
2025-05-23 16:19:11 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// '/' 경로로 GET 요청이 왔을 때 실행될 함수를 정의합니다.
+app.get('/', (req, res) => {
+  res.send('Hello, Express with TypeScript (ESM)!');
+});
+
+
+// 설정한 포트(3000번)에서 서버를 시작합니다.
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+  console.log('웹 브라우저에서 이 주소로 접속해 보세요!');
+});
